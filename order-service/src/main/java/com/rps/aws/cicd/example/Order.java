adf65c7b84660472a4ccb5aae0467902081b530a
SANDEEP <SANDEEP@DESKTOP-7G2JT9N>
2021-06-07 07:43:34 +0530

    first commit

---
+++ order-service/src/main/java/com/rps/aws/cicd/example/Order.java
@@ -0,0 +1,64 @@
+package com.rps.aws.cicd.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+public class Order {
+
+	private int id;
+	private String name;
+	private int quantity;
+	private long price;
+
+	public Order() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Order(int id, String name, int quantity, long price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.quantity = quantity;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public long getPrice() {
+		return price;
+	}
+
+	public void setPrice(long price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", name=" + name + ", quantity=" + quantity + ", price=" + price + "]";
+	}
+
+}
